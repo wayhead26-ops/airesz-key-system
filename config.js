56e90263eb4090e1775425dd3874f95de21532e7
wayhead26-ops <wayhead26@gmail.com>
2026-08-02 19:06:07 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.AIRESZ_CONFIG = Object.freeze({
   workerUrl: "https://airesz-key-api.airesz-key-system.workers.dev",
-  supportUrl: "https://discord.gg/5hazzAq7B"
+  supportUrl: "https://discord.gg/xkb2P6gnE"
 });
